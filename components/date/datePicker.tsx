79090e93c45603b01118477d30896b8303521280
alfitosantosa <sanskun02pg@gmail.com>
2026-05-09 14:30:31 +0700

    Upgrade core dependencies (React, Next.js, Prisma, TanStack Query, etc) - Remove deprecated react-day-picker props and update Calendar component

--- components/date/datePicker.tsx
+++ components/date/datePicker.tsx
@@ -24,17 +24,21 @@ export function DatePickerWithRange({ date: controlledDate, setDate: setControll
         <PopoverTrigger asChild>
           <Button id="date" variant={"outline"} className={cn("w-75 justify-start text-left font-normal", !date && "text-muted-foreground")}>
             <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ?
-              date.to ?
+            {date?.from ? (
+              date.to ? (
                 <>
                   {format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}
                 </>
-              : format(date.from, "LLL dd, y")
-            : <span>Pick a date</span>}
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0" align="start">
-          <Calendar initialFocus mode="range" defaultMonth={date?.from} selected={date} onSelect={setDate} numberOfMonths={2} />
+          <Calendar mode="range" defaultMonth={date?.from} selected={date} onSelect={setDate} numberOfMonths={2} />
         </PopoverContent>
       </Popover>
     </div>
