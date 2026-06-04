b38650e0a7f1779262fc25bf00d7bd5685fb4a9d
alfitosantosa <sanskun02pg@gmail.com>
2026-06-05 02:10:26 +0700

    fix:fixing deps prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import type { PrismaConfig } from "prisma";
+import { env } from "prisma/config";
 
 export default {
   schema: "./prisma/schema.prisma",
   migrations: {
     path: "./prisma/migrations",
-    // seed: 'tsx prisma/seed.ts',
+    seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: env("DATABASE_URL"),
   },
 } satisfies PrismaConfig;
